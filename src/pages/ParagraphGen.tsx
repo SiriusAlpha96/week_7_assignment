5cc115b128eb35ce2658523daa14b960dfbbd4a0
SiriusAlpha96 <shreyama9670@gmail.com>
2024-04-28 00:45:55 +0530

    initial commit

---
+++ src/pages/ParagraphGen.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+
+export default function ParagraphGen() {
+  return (
+    <div>
+        <div className="w-full h-screen font-medium text-gray-700">ParagraphGen</div>
+
+    </div>
+  )
+}
